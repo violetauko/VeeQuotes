1fb157dc482959be8f7d4e7cc3b1c58734cbb95b
violetauko <aukoviolet24@gmail.com>
2022-04-12 01:43:29 +0300

    date constructor

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,9 +1,10 @@
 export class Quote {
-    constructor(public id:number,public quote:string,public author:string,public upvotes:number,public downvotes:number){
+    constructor(public id:number,public quote:string,public author:string,public upvotes:number,public downvotes:number, public postDate:Date){
     this.id=id;
     this.quote=quote;
     this.upvotes=upvotes;
     this.downvotes=downvotes;
     this.author=author;
+    this.postDate=postDate
     }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,9 +9,9 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[]=[
-    new Quote(1, "I hated every minute of training, but I said, 'Don't quit. Suffer now andlive the rest of your life as a champion.'","Muhammed Ali",0,0),
-    new Quote(2, "When we are no longer able to change a situation - we are challenged to change ourselves."," Viktor E. Frankl",0,0),
-    new Quote(3, "Progress is impossible without change, and those who cannot change their minds cannot change anything.", "George Bernard Shaw",0,0),
+    new Quote(1, "I hated every minute of training, but I said, 'Don't quit. Suffer now andlive the rest of your life as a champion.'","Muhammed Ali",0,0,new Date(2022,4,12)),
+    new Quote(2, "When we are no longer able to change a situation - we are challenged to change ourselves."," Viktor E. Frankl",0,0,new Date(2022,4,12)),
+    new Quote(3, "Progress is impossible without change, and those who cannot change their minds cannot change anything.", "George Bernard Shaw",0, 0, new Date(2022,4,12))
   ]
 
   arr: number[]=this.quotes.map(quote=>quote.upvotes)
